3e56584a2697b45ec190236431fcb8ebed7204f9
cuda05 <cuda05@boada-1.ac.upc.es>
2016-06-22 00:01:59 +0200

    Added report

--- run_face_detect_seq.sh
+++ run_face_detect_seq.sh
@@ -3,5 +3,5 @@
 . common.sh
 NAME=face_detect_seq
 make bin/${NAME}.exe
-#qsub -l cuda job.sh $NAME
-./bin/${NAME}.exe ./images/input.png
+qsub -l cuda job.sh $NAME
+#./bin/${NAME}.exe ./images/input.png
